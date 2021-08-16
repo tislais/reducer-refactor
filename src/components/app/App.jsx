ed7fb2a5321e39f3e1c015ed8f6a3d8441904e68
Tis Lais <tistististis@gmail.com>
2021-08-16 16:41:05 -0700

    Initial commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const useRecord = (init) => {
+  const [before, setBefore] = useState([]);
+  const [current, setCurrent] = useState(init);
+  const [after, setAfter] = useState([]);
+
+  const undo = () => {
+    setAfter((after) => [current, ...after]);
+    setCurrent(before[before.length - 1]);
+    setBefore((before) => before.slice(0, -1));
+  };
+
+  const redo = () => {
+    setBefore((before) => [...before, current]);
+    setCurrent(after[0]);
+    setAfter((after) => after.slice(1));
+  };
+
+  const record = (val) => {
+    setBefore((before) => [...before, current]);
+    setCurrent(val);
+  };
+
+  return {
+    undo,
+    record,
+    redo,
+    current,
+  };
+};
+
+function App() {
+  const { current, undo, redo, record } = useRecord("#FF0000");
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input
+        type="color"
+        value={current}
+        onChange={({ target }) => record(target.value)}
+      />
+      <div
+        style={{ backgroundColor: current, width: "10rem", height: "10rem" }}
+      ></div>
+    </>
+  );
+}
+
+export default App;
