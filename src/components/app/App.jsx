09f84767e8fa2bcdf94b3ebdd5f47626f0b63cdb
Tis Lais <tistististis@gmail.com>
2021-08-17 11:22:43 -0700

    Clean up old code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,38 +2,7 @@ import React, { useReducer } from 'react';
 import { record, undo, redo } from '../state/actions';
 import { initialState, reducer } from '../state/reducer';
 
-// const useRecord = (init) => {
-//   const [before, setBefore] = useState([]);
-//   const [current, setCurrent] = useState(init);
-//   const [after, setAfter] = useState([]);
-
-//   const undo = () => {
-//     setAfter((after) => [current, ...after]);
-//     setCurrent(before[before.length - 1]);
-//     setBefore((before) => before.slice(0, -1));
-//   };
-
-//   const redo = () => {
-//     setBefore((before) => [...before, current]);
-//     setCurrent(after[0]);
-//     setAfter((after) => after.slice(1));
-//   };
-
-//   const record = (val) => {
-//     setBefore((before) => [...before, current]);
-//     setCurrent(val);
-//   };
-
-//   return {
-//     undo,
-//     record,
-//     redo,
-//     current,
-//   };
-// };
-
 function App() {
-  // const { current, undo, redo, record } = useRecord("#FF0000");
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
