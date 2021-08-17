0b41cb498fc7b876886f30fb5c657c0f2d3233af
Tis Lais <tistististis@gmail.com>
2021-08-16 18:28:25 -0700

    Add useDispatch hook

--- src/components/state/ReduxProvider.jsx
+++ src/components/state/ReduxProvider.jsx
@@ -1,6 +1,11 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 
 const ReduxContext = createContext();
+
+export const useDispatch = () => {
+  const { dispatch } = useContext(ReduxContext);
+  return dispatch;
+};
 
 export const ReduxProvider = ({ reducer, initialState, children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
