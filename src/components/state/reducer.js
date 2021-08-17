8e50c1a67b498c1a69f7ae0e585c0ad298c0c598
Tis Lais <tistististis@gmail.com>
2021-08-16 18:27:51 -0700

    Add get current color action to reducer

--- src/components/state/reducer.js
+++ src/components/state/reducer.js
@@ -1,7 +1,11 @@
-export const initialState = {};
+export const initialState = {
+  current: '',
+};
 
 export function reducer(state, action) {
   switch(action.type) {
+    case 'GET_CURRENT':
+      return { ...state, current: action.payload };
     default:
       return state;
   }
